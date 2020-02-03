0b60478aadd794ff01faed61be0feee9a886266f
Radosław Radomski <pryq11@gmail.com>
2020-02-03 10:32:17 +0100

    Added logging with serilog

--- ProductionDocumentationServer/Program.cs
+++ ProductionDocumentationServer/Program.cs
@@ -1,5 +1,8 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace ProductionDocumentationServer
 {
@@ -7,11 +10,17 @@ namespace ProductionDocumentationServer
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().
+                Enrich.FromLogContext().
+                WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error).
+                WriteTo.Console(Serilog.Events.LogEventLevel.Information).
+                CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
